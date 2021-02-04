905d4c83bc37ec603e027f8ce090340071298bfb
RafaelBarrero <rafael.barrero@bosonit.com>
2021-02-04 10:41:18 +0100

    __init__

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.calcular_cuadrado_array import CalcularCuadradoArray
 from swagger_server.models.calcular_cuadrado_response import CalcularCuadradoResponse
+from swagger_server.models.calcular_media_csv_url import CalcularMediaCsvUrl
 from swagger_server.models.calcular_media_post import CalcularMediaPost
 from swagger_server.models.calcular_media_response200 import CalcularMediaResponse200
 from swagger_server.models.error import Error
